2bea58591935149f4af5c073ee5fa64cde3db37c
Iswan Jumat <info@iswanjumat.com>
2023-03-09 00:12:21 +0800

    initial commit

---
+++ src/data/index.js
@@ -0,0 +1,5 @@
+export const CATEGORIES = [
+  {name: 'Animals', words: ['Dog', 'Cat', 'Elephant', 'Lion', 'Tiger']},
+  {name: 'Cities', words: ['New York', 'London', 'Paris', 'Tokyo', 'Sydney']},
+  {name: 'Food', words: ['Pizza', 'Burger', 'Sushi', 'Pasta', 'Steak']},
+];
